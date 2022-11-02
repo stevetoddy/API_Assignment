27c0f8f1b6b9a31a12eafb52054875984425152a
stevetoddy <stevetoddy@gmail.com>
2022-11-02 11:16:16 +1100

    Start framework code

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from flask import Flask
 from init import db, ma
 import os
 
-def create_app():
-    # Instantiating our Flask app as app
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # Getting the Database URI from my global variables file to link database
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-    # Pulling db from init.py to instantiate the SQLAlchemy db model and passing in our flask app 
-    db.init_app(app)
-    ma.init_app(app)
+@app.route('/')
+def index():
+    return 'Hello world'
+
